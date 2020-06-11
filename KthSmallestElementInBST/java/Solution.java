1a6fec7a11b8c38dc71a8be581b6ba9df080be7e
Zhoodar <ajoodar@gmail.com>
2020-06-11 16:42:44 +0600

    solution without extra space, using in order index of the BST

--- KthSmallestElementInBST/java/Solution.java
+++ KthSmallestElementInBST/java/Solution.java
@@ -1,15 +1,25 @@
 class Solution {
+    
+    /**
+    * Time complexity: O(n) n = number of nodes in BST
+    * Space complexity: O(h) h = height of the Tree
+    */
+    private int inOrderIndex = 0;
+    private int result = -1;
+    
     public int kthSmallest(TreeNode root, int k) {
-        List<Integer> list = new ArrayList<>();
-        dfs(root, list);
-        return list.get(k-1);
+        return dfs(root, k);
     }
     
-    private List<Integer> dfs(TreeNode node, List<Integer> list) {
+    private int dfs(TreeNode node, int k) {
         if (node == null) {
-            return list;
+            return result;
         }
-        dfs(node.left, list).add(node.val);
-        return dfs(node.right, list);
+        dfs(node.left, k);
+        if (++inOrderIndex == k) {
+            result = node.val;
+            return result;
+        }
+        return dfs(node.right, k);
     }
 }
